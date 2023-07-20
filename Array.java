20b9feb474e88d99ac235015966bee0a2f429a31
Shubhamtest96 <subhamtest96@gmail.com>
2023-07-20 19:50:21 +0530

    code on array in java

---
+++ Array.java
@@ -0,0 +1,15 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Array
+{
+	public static void main(String[] args) {
+	    int a [] = {10,20,30,40};
+		System.out.println(a[3]);
+	}
+}
